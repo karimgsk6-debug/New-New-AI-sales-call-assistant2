feebbf4df0d5ad14d880bb71ce2ab4c3d8cb5a9b
karimgsk6-debug <karim.gsk6@gmail.com>
2025-09-25 17:18:55 +0300

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -60,7 +60,7 @@ st.markdown(f"""
     background-size: cover;
 }}
 .title-box {{
-    background: rgba(275,275,275,0.8);
+    background: rgba(275,275,275,0.5);
     backdrop-filter: blur(10px);
     padding: 15px;
     border-radius: 12px;
