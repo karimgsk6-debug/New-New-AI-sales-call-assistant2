6283538d35c199e05a0521bceb006c5c8a50dd2a
karimgsk6-debug <karim.gsk6@gmail.com>
2025-09-25 16:48:00 +0300

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -37,7 +37,7 @@ if "uploaded_pdf_text" not in st.session_state:
 # ----------------------------
 # Background image
 # ----------------------------
-BACKGROUND_URL = "https://img.freepik.com/premium-photo/black-woman-smiling-using-phone-with-yellow-background_176841-18605.jpg"
+BACKGROUND_URL = "https://chatgpt.com/s/m_68d547f40cbc8191bbf2511f2d988763"
 
 # Get average brightness for text/button color adjustment
 def get_brightness(url):
