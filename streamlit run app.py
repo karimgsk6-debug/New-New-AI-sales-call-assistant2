e790be6a911ddd299cdbdc5bd49c1dc46ba7de9c
karimgsk6-debug <karim.gsk6@gmail.com>
2025-09-25 14:55:14 +0300

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -19,7 +19,7 @@ except ImportError:
 client = Groq(api_key="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")  # 🔑 Insert your Groq API key here
 
 # --- Background image ---
-bg_url = "https://media.istockphoto.com/id/1179600920/photo/smiling-teenage-girl-holding-digital-tablet-and-looking-to-camera.jpg?s=612x612&w=0&k=20&c=BhjFX05s17kS2K8X5Jm4jhMwNQKQ1RdsnGR_-tcy9nI="
+bg_url = "https://www.shutterstock.com/image-photo/excited-girl-white-shirt-using-260nw-708132598.jpg"
 st.markdown(
     f"""
     <style>
