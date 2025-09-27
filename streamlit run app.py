d0af2c85a26eb1c78d63958c1e7628513519d01e
karimgsk6-debug <karim.gsk6@gmail.com>
2025-09-27 18:24:41 +0300

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -74,7 +74,7 @@ CSS = f"""
 /* Main background */
 .stApp {{
     background: url('{BACKGROUND_URL}') no-repeat top right;
-    background-size: auto 120%;
+    background-size: auto 125%;
     background-attachment: fixed;
     transition: background-size 0.3s ease;
 }}
