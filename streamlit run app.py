d3d4dbe73385ba6fe0ffc3535288e362f8981eca
karimgsk6-debug <karim.gsk6@gmail.com>
2025-09-27 17:35:35 +0300

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -52,7 +52,7 @@ if "pdf_summary" not in st.session_state:
 # ----------------------------
 BACKGROUND_URL = ("https://sdmntpreastus.oaiusercontent.com/files/00000000-d8fc-61f9-a032-8b22cd0506d4/raw?se=2025-09-27T15%3A26%3A11Z&sp=r&sv=2024-08-04&sr=b&scid=94806956-ceed-5d91-8345-e5c58fe80700&skoid=6658dbdd-f305-4d30-8f6b-d62218202cb9&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-09-26T17%3A00%3A28Z&ske=2025-09-27T17%3A00%3A28Z&sks=b&skv=2024-08-04&sig=H0sSlc9j5wxHuYfQPjkJwkd417JLlDNhIgpE9eK4xp4%3D")
 
-GSK_LOGO_URL = "https://www.tungsten-network.com/wp-content/uploads/2020/05/GSK_Logo_Full_Colour_RGB.png"
+GSK_LOGO_URL = "https://www.stevenagecatalyst.com/wp-content/uploads/2024/04/MicrosoftTeams-image-14.png"
 
 # compute brightness to pick text color
 def get_brightness(url):
