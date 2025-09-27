259bf74d1c4b2f9bb9a1332bd1618d8f8dbfacb6
karimgsk6-debug <karim.gsk6@gmail.com>
2025-09-28 00:18:18 +0300

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -91,11 +91,11 @@ CSS = f"""
 .gsk-logo {{
     position: fixed;
     top: 60px;
-    right: 16px;
+    left: 16px;
     z-index: 1000;
 }}
 .title-box {{
-    background: rgba(255,255,255,0.96);
+    background: rgba(255,255,255,0.6);
     padding: 28px;
     border-radius: 14px;
     text-align: center;
