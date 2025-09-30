84c36d51972beca14f5873922e5a923661752a3d
karimgsk6-debug <karim.gsk6@gmail.com>
2025-09-30 22:22:40 +0300

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -6,7 +6,7 @@ import re
 # ----------------------------
 # Settings
 # ----------------------------
-BACKGROUND_URL = "https://drive.google.com/uc?id=1WlvNx4MqufxuGUw9ilLxGJLsuozbX17b"
+BACKGROUND_URL = "https://drive.usercontent.google.com/download?id=1WlvNx4MqufxuGUw9ilLxGJLsuozbX17b&authuser=0"
 
 sales_call_steps = [
     "Greet",
