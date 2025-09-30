4aa2dd5328eed99c1c53f76995698d097351105b
karimgsk6-debug <karim.gsk6@gmail.com>
2025-10-01 01:56:49 +0300

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -34,7 +34,7 @@ st.markdown(f"""
 <style>
 .stApp {{
     background: url("{BACKGROUND_URL}") no-repeat right top fixed;
-    background-size: auto 120%;
+    background-size: auto 130%;
 }}
 
 .title-box {{
