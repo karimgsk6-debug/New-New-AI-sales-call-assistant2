ed6ba1258b1e35f15f1655ba9b0cf9646f947068
karimgsk6-debug <karim.gsk6@gmail.com>
2025-10-02 12:54:24 +0300

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -119,7 +119,7 @@ CSS = f"""
   overflow-y: auto;
   padding: 12px;
   border-radius: 10px;
-  background: rgba(255,255,255,0.7);
+  background: rgba(255,255,255,0.6);
   margin-bottom: 20px;
 }}
 
