6236dccef11ce0edaceb7b201088d1abc4fb3c20
karimgsk6-debug <karim.gsk6@gmail.com>
2025-10-19 22:54:20 +0300

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -68,7 +68,7 @@ if "main_input" not in st.session_state:
     st.session_state.main_input = ""
 
 # ---------------------------- Assets ----------------------------
-BACKGROUND_URL = "https://raw.githubusercontent.com/karimgsk6-debug/New-New-AI-sales-call-assistant2/main/.devcontainer/Background1.jpeg"
+BACKGROUND_URL = "https://github.com/karimgsk6-debug/New-New-AI-sales-call-assistant2/blob/3d1d78cf288cca4465d7759148fb705ff51d7ac3/.devcontainer/Background1.jpeg"
 GSK_LOGO_URL = "https://raw.githubusercontent.com/karimgsk6-debug/New-New-AI-sales-call-assistant2/main/.devcontainer/gsk-logo.png"
 AI_LOGO_URL = "https://raw.githubusercontent.com/karimgsk6-debug/New-New-AI-sales-call-assistant2/main/.devcontainer/ai-logo.png"
 
