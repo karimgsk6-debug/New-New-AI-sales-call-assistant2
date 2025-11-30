14d39263cfd37399f716a4275c4b05cc4ce3fc7d
karimgsk6-debug <karim.gsk6@gmail.com>
2025-11-30 16:12:32 +0200

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -129,7 +129,12 @@ brand_data = globals().get("brand_data", st.session_state.get("brand_data", {}))
 if not brand_data:
     brand_data = {"Default": {"module":"No data loaded"}}
 
-sel_brand = st.sidebar.selectbox("Brand", list(brand_data.keys()), index=0("Brand", list(brand_data.keys()), index=list(brand_data.keys()).index(st.session_state.selected_brand))
+# Brand selector (fixed syntax)
+sel_brand = st.sidebar.selectbox(
+    "Brand",
+    list(brand_data.keys()),
+    index=0
+)), index=0("Brand", list(brand_data.keys()), index=list(brand_data.keys()).index(st.session_state.selected_brand))
 st.session_state.selected_brand = sel_brand
 bconf = brand_data[sel_brand]
 refs_text = gather_text_from_folder(bconf['references_path'])
