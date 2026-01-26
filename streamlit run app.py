ab31ca3a1b508fce1e853f9ccf91cfda59ded649
karimgsk6-debug <karim.gsk6@gmail.com>
2026-01-26 12:40:07 +0200

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -25,7 +25,7 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY","gsk_rsoppklsXlzgSHCXIW8kWGdyb3FY
 # VISUAL ASSETS
 # ==========================================================
 AI_AVATAR = "https://raw.githubusercontent.com/karimgsk6-debug/New-New-AI-sales-call-assistant2/main/.devcontainer/Visuals/futuristic_hologram_ai.gif"
-BACKGROUND_PATH = ".devcontainer/Visuals/MR mentor final1.png"
+BACKGROUND_PATH = ".devcontainer/Visuals/MR mentor final.png"
 
 def set_background(image_path):
     if os.path.exists(image_path):
